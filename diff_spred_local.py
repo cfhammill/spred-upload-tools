6cc47c495905eae1805a85a2f5a9218134b32d6e
Michael Joseph <josephmje.22@gmail.com>
2017-02-14 11:25:16 -0500

    Update diff_spred_local.py

--- diff_spred_local.py
+++ diff_spred_local.py
@@ -1,65 +1,67 @@
 #!/usr/bin/python
 
 import os
-import csv
-import json
 import itertools
 import pyxnat
 
 # Which MR sessions for each subject haven't been uploaded to SPReD yet?
 
-# 1. CREATE VARIABLE OF MR SESSIONS IN LOCAL MR160 DIRECTORY
+# 1. IDENTIFY MR SESSIONS IN LOCAL MR160 DIRECTORY
 
-# Find all MR Session subdirectories
-sessions_Raw = os.listdir('/micehome/mjoseph/data8/mrdata/MR160')
-sessions = []
-Tags = ('MR160-088', 'MR160-105', 'MR160-113', 'MR160-114')
-for names in sessions_Raw:
-    if names.startswith(Tags):    
-        sessions.append(names)
+# find all MR Session subdirectories
+MR160local_raw  = os.listdir('/micehome/mjoseph/data8/mrdata/MR160')
 
-# Create empty lists of each variable of interest to build
+# remove backups, withdrawn sessions, tests and QC scans
 MR160local = []
-for row in sessions:
-    [Study, Site, Subj, Visit] = row.split('-')
-    if Site=='088':
-        SiteCode = 'HSC'
-    else:
-        SiteCode = 'HBK'
+Tags = ('MR160-088', 'MR160-105', 'MR160-113', 'MR160-114')
+for names in MR160local_raw:
+    if names.startswith(Tags):    
+        MR160local.append(names)
 
-    subjectID = 'PND03_%s_%s' % (SiteCode,Subj)
-        
-    sessionSuffix = '%s_SE01_MR' % (Visit)
-    sessionID = '%s_%s' % (subjectID, sessionSuffix)
-    MR160local.append(sessionID)
+# 2. IDENTIFY MR SESSIONS ALREADY UPLOADED TO SPReD
 
-# 2. CREATE VARIABLE OF MR SESSIONS ALREADY UPLOADED TO SPReD
+spred = pyxnat.Interface(server='https://spred.braincode.ca/spred')
 
-spred_url = 'https://spred.braincode.ca/spred'
-
-credentials = {}
-with open('/micehome/mjoseph/Documents/Code/credentials.txt', 'r') as f:
-    for line in f:
-        user, pwd = line.strip().split(':')
-        credentials[user] = pwd 
-
-spred = pyxnat.Interface(server=spred_url, user=user, password=pwd)
-
-session_label = spred.select('xnat:mrSessionData',['xnat:mrSessionData/XNAT_COL_MRSESSIONDATALABEL']).all()
+MR160spredJSON = spred.select('xnat:mrSessionData',['xnat:mrSessionData/XNAT_COL_MRSESSIONDATALABEL']).all()
 # outputs as a json table
 
-# convert to list
-newSessionLabel = session_label.as_list()
+# pyxnat has its own set of arguments to convert json to list of lists
+MR160spredList = MR160spredJSON.as_list()
 
 #unpack list and remove header
 MR160spred = list(itertools.chain(*newSessionLabel[1:]))
 
+# convert MR160spred labeling to MR160local
+MR160spredmod = []
+for row in MR160spred:
+    if len(row.split('_')) == 6:
+        [Study, SiteCode, Subj, Visit, SessionNum, SessionType] = row.split('_')
+        if SiteCode=='HSC':
+            Site = '088'
+        else:
+            Site = '105'
+        sessionID = 'MR160-%s-%s-%s' % (Site, Subj, Visit)
+    else: 
+        [Study, Site, Subj, Visit, SessionNum, SessionType, Deleted] = row.split('_')
+        if SiteCode=='HSC':
+            Site = '088'
+        else:
+            Site = '105'        
+        sessionID = 'MR160-%s-%s-%s-deleted' % (Site, Subj, Visit)
+    MR160spredmod.append(sessionID)
+
 # 3. FIND DIFFERENCE BETWEEN MR SESSIONS IN LOCAL MR160 DIRECTORY and SPReD
 
 # Retrieves all MR sessions that haven't been uploaded yet
-spredToDo = list(set(MR160local) - set(MR160spred))
+spredToDo = list(set(MR160local) - set(MR160spredmod))
 
 # Retrieves all the deleted MR sessions from SPReD; but also found PND03_HBK_0446_02_SE01_MR
-list(set(MR160spred) - set(MR160local))
+spredDeleted = list(set(MR160spredmod) - set(MR160local))
 
-# instead of converting spredToDo back to original form, convert pyxnat to directory structure
+print "The following MR sessions haven't been uploaded to SPReD yet:"
+for i in spredToDo:
+    print i
+
+print "The following MR sessions have been deleted from SPReD:"
+for j in spredDeleted:
+    print j
